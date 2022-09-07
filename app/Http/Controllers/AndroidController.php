bd61885d621628a8d87c95be7df5974e7e15e6c4
n0k1b <nokibevon7@gmail.com>
2022-09-07 19:19:05 +0600

    base url change

--- app/Http/Controllers/AndroidController.php
+++ app/Http/Controllers/AndroidController.php
@@ -35,7 +35,7 @@ use DB;
 class AndroidController extends Controller
 {
     //
-    public $base_url = "http://gogoshopbd.com/";
+    public $base_url = "http://admin.feerot.com/";
     protected $current_date;
 
      public function __construct()
