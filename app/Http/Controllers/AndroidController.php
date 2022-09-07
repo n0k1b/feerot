7bd929b25b34425a4a27fcf0ef2d63dc6ecfa721
n0k1b <nokibevon7@gmail.com>
2022-09-07 21:22:07 +0600

    image path updated

--- app/Http/Controllers/AndroidController.php
+++ app/Http/Controllers/AndroidController.php
@@ -35,7 +35,7 @@ use DB;
 class AndroidController extends Controller
 {
     //
-    public $base_url = "http://admin.feerot.com/";
+    public $base_url = "http://admin.feerot.com/public/";
     protected $current_date;
 
      public function __construct()
