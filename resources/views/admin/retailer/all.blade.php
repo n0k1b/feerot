725bb661febe69865a86ba2b4c2cba9f529e0b83
n0k1b <nokibevon7@gmail.com>
2022-10-01 11:57:15 +0600

    All retailer name update

--- resources/views/admin/retailer/all.blade.php
+++ resources/views/admin/retailer/all.blade.php
@@ -16,7 +16,7 @@
     <div class="row page-titles mx-0">
         <div class="col-sm-6 p-md-0">
             <div class="welcome-text">
-                <h4>All Brand</h4>
+                <h4>All Retailer</h4>
             </div>
         </div>
         <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
