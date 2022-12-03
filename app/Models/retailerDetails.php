759ddbc8164de4a14e15bc6507b1c70aca89fbc2
n0k1b <nokibevon7@gmail.com>
2022-12-03 20:25:19 +0600

    Banner image added

--- app/Models/retailerDetails.php
+++ app/Models/retailerDetails.php
@@ -10,16 +10,11 @@ class retailerDetails extends Model
 {
     use HasFactory, SoftDeletes;
     protected $guarded = [];
-    protected $appends = ['thumbnail_image'];
     protected $base_url = 'https://admin.feerot.com/public/';
 
     public function user()
     {
         return $this->belongsTo('App\Models\User', 'user_id', 'id');
-    }
-    public function getThumbnailImageAttribute()
-    {
-        return $this->base_url . $this->thumbnail_image;
     }
 
 }
